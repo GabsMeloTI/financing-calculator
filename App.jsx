b64cca67cd022e7b8dd9dee7ddd29520734fc961
gabsmelin <rm550202@fiap.com.br>
2024-03-20 22:23:08 -0300

    Acabamos de criar toda lógica do projeto, agora só falta organiza-ló, cada function na sua page.

--- App.jsx
+++ App.jsx
@@ -1,8 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, Modal } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
 
-export default function App() {
+
+
+function TelaInicial({ navigation }) {
+  const [modalVisible, setModalVisible] = useState(false);
+
   const [nome, setNome] = useState('');
   const [cpf, setCPF] = useState('');
   const [email, setEmail] = useState('');
@@ -56,10 +63,33 @@ export default function App() {
             style={styles.input}
           />
 
-          <Button title="Confirmar"/>
+          <Button title="Confirmar" onPress={() => setModalVisible(true)}/>
       </View>
 
-      
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(!modalVisible)}
+      >
+          <View>
+            <Text>Olá senhor {nome}, cujo CPF é {cpf}. Temos algumas opções de simulação de parcelas, veja qual se encaixa melhor ao seu objetivo.</Text>
+            
+            <Button title="Calculo sem entrada" onPress={() => {
+              navigation.navigate('CalculoSemEntrada');
+              setModalVisible(false);
+            }}/>
+            <Button title="Calculo com entrada" onPress={() => {
+              navigation.navigate('CalculoComEntrada');
+              setModalVisible(false);
+            }}/>
+            <Button title="Calculo parcelas iguais" onPress={() => {
+              navigation.navigate('CalculoParcelasIguais');
+              setModalVisible(false);
+            }}/>
+          </View>
+      </Modal>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -93,3 +123,288 @@ const styles = StyleSheet.create({
     fontSize: 36,
   }
 }); 
+
+
+
+
+function CalculoSemEntrada() {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [juros, setJuros] = useState('');
+  const [numParcelas, setNumParcelas] = useState('');
+  const [coeFinanceiro, setCoeFinanceiro] = useState('');
+  const [valorFinanciamento, setValorFinanciamento] = useState('');
+  const [valorParcela, setValorParcela] = useState('');
+  const [resultadoCalculo, setResultadoCalculo] = useState(null);
+
+  function calculo(juros, numParcelas) {
+    const cf = (juros / 100) / (1 - 1 / Math.pow(1 + (juros / 100), numParcelas)); 
+    setCoeFinanceiro(cf);
+
+    const valorParcelaCalculado = cf * valorFinanciamento;
+    setValorParcela(valorParcelaCalculado); 
+
+    const resultado = valorParcelaCalculado * numParcelas; 
+    setResultadoCalculo(resultado); 
+  }
+
+  return (
+    <View style={styles.formulario}>
+          <Text style={styles.label}>Número de Parcelas:</Text>
+          <TextInput
+            placeholder='Informe o número de parcelas'
+            value={numParcelas}
+            keyboardType="double"
+            onChangeText={(text) => {
+              const numericValue = text.replace(/[^0-9]/g, ''); 
+              setNumParcelas(numericValue);
+            }}
+            style={styles.input}
+          />
+
+          <Text style={styles.label}>Valor do financiamento:</Text>
+          <TextInput
+            placeholder='Informe o valor do seu financiamento'
+            value={valorFinanciamento}
+            onChangeText={(text) => {
+              const numericValue = text.replace(/[^0-9]/g, ''); 
+              setValorFinanciamento(numericValue);
+            }}
+            style={styles.input}
+          />
+
+          <Text style={styles.label}>Juros:</Text>
+          <TextInput
+            placeholder='Informe os possíveis juros'
+            value={juros}
+            keyboardType="numeric"
+            onChangeText={(text) => {
+              const numericValue = text.replace(/[^0-9.]/g, ''); 
+              setJuros(numericValue);
+            }}
+            style={styles.input}
+          />
+
+          <Button title="Calcular"  onPress={() => {
+              calculo(juros, numParcelas);
+              setModalVisible(true);
+            }}/> 
+
+          <Modal
+            animationType="slide"
+            transparent={false}
+            visible={modalVisible}
+            onRequestClose={() => setModalVisible(!modalVisible)}
+          >
+            <View>
+                <Text style={styles.label}>Valor total do financiamento: R${resultadoCalculo}</Text> 
+                <Text style={styles.label}>Valor das Parcelas: R${valorParcela}</Text> 
+                <Text style={styles.label}>Valor do juros: R${resultadoCalculo - valorFinanciamento}</Text> 
+            </View>
+          </Modal>
+      </View>
+  );
+}
+
+function CalculoComEntrada() {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [juros, setJuros] = useState('');
+  const [numParcelas, setNumParcelas] = useState('');
+  const [coeFinanceiro, setCoeFinanceiro] = useState('');
+  const [valorEntrada, setValorEntrada] = useState(0);
+  const [valorFinanciamento, setValorFinanciamento] = useState('');
+  const [valorParcela, setValorParcela] = useState('');
+  const [resultadoCalculo, setResultadoCalculo] = useState(null);
+
+  function calculo(juros, numParcelas) {
+    const cf = (juros / 100) / (1 - 1 / Math.pow(1 + (juros / 100), numParcelas)); 
+    setCoeFinanceiro(cf);
+
+    const valorParcelaCalculado = cf * (valorFinanciamento - valorEntrada);
+    setValorParcela(valorParcelaCalculado); 
+
+    const resultado = valorParcelaCalculado * numParcelas; 
+    const resultadoTotal = parseFloat(resultado) + parseFloat(valorEntrada);
+    setResultadoCalculo(resultadoTotal);
+  }
+
+  return (
+    <View style={styles.formulario}>
+
+          <Text style={styles.label}>Valor do financiamento:</Text>
+          <TextInput
+            placeholder='Informe o valor do seu financiamento'
+            value={valorFinanciamento}
+            onChangeText={(text) => {
+              const numericValue = text.replace(/[^0-9]/g, ''); 
+              setValorFinanciamento(numericValue);
+            }}
+            style={styles.input}
+          />
+
+          <Text style={styles.label}>Valor da Entrada:</Text>
+          <TextInput
+            placeholder='Informe o valor da entrada'
+            value={valorEntrada}
+            keyboardType="numeric"
+            onChangeText={(text) => {
+              const numericValue = text.replace(/[^0-9]/g, ''); 
+              setValorEntrada(numericValue);
+            }}
+            style={styles.input}
+          />
+
+          <Text style={styles.label}>Número de Parcelas:</Text>
+          <TextInput
+            placeholder='Informe o número de parcelas'
+            value={numParcelas}
+            keyboardType="numeric"
+            onChangeText={(text) => {
+              const numericValue = text.replace(/[^0-9]/g, ''); 
+              setNumParcelas(numericValue);
+            }}
+            style={styles.input}
+          />
+
+          <Text style={styles.label}>Juros:</Text>
+          <TextInput
+            placeholder='Informe os possíveis juros'
+            value={juros}
+            keyboardType="numeric"
+            onChangeText={(text) => {
+              const numericValue = text.replace(/[^0-9.]/g, ''); 
+              setJuros(numericValue);
+            }}
+            style={styles.input}
+          />
+
+          <Button title="Calcular"  onPress={() => {
+              calculo(juros, numParcelas);
+              setModalVisible(true);
+            }}/> 
+
+          <Modal
+            animationType="slide"
+            transparent={false}
+            visible={modalVisible}
+            onRequestClose={() => setModalVisible(!modalVisible)}
+          >
+            <View>
+                <Text style={styles.label}>Valor total do financiamento: R${resultadoCalculo}</Text> 
+                <Text style={styles.label}>Valor da entrada: R${valorEntrada}</Text> 
+                <Text style={styles.label}>Valor das Parcelas: R${valorParcela}</Text> 
+                <Text style={styles.label}>Valor do juros: R${resultadoCalculo - valorFinanciamento}</Text> 
+            </View>
+          </Modal>
+      </View>
+  );
+}
+
+function CalculoParcelasIguais() {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [juros, setJuros] = useState('');
+  const [numParcelas, setNumParcelas] = useState('');
+  const [coeFinanceiro, setCoeFinanceiro] = useState('');
+  const [valorEntrada, setValorEntrada] = useState('');
+  const [valorFinanciamento, setValorFinanciamento] = useState('');
+  const [valorParcela, setValorParcela] = useState('');
+  const [resultadoCalculo, setResultadoCalculo] = useState(null);
+
+  function calculo(juros, numParcelas) {
+    const cf = (juros / 100) / (1 - 1 / Math.pow(1 + (juros / 100), numParcelas)); 
+    setCoeFinanceiro(cf);
+
+    const valorParcelaCalculado = (valorFinanciamento * cf) / (1 + cf);
+    setValorParcela(valorParcelaCalculado); 
+
+    const resultado = valorEntrada + (valorParcelaCalculado * numParcelas); 
+    setResultadoCalculo(resultado);
+  }
+
+  return (
+    <View style={styles.formulario}>
+
+          <Text style={styles.label}>Valor do financiamento:</Text>
+          <TextInput
+            placeholder='Informe o valor do seu financiamento'
+            value={valorFinanciamento}
+            keyboardType="numeric"
+            onChangeText={(text) => {
+              const numericValue = text.replace(/[^0-9.]/g, ''); 
+              setValorFinanciamento(numericValue);
+            }}
+            style={styles.input}
+          />
+
+          <Text style={styles.label}>Valor da Entrada:</Text>
+          <TextInput
+            placeholder='Informe o valor da entrada'
+            value={valorEntrada}
+            keyboardType="numeric"
+            onChangeText={(text) => {
+              const numericValue = text.replace(/[^0-9.]/g, ''); 
+              setValorEntrada(numericValue);
+            }}
+            style={styles.input}
+          />
+
+          <Text style={styles.label}>Número de Parcelas:</Text>
+          <TextInput
+            placeholder='Informe o número de parcelas'
+            value={numParcelas}
+            keyboardType="numeric"
+            onChangeText={(text) => {
+              const numericValue = text.replace(/[^0-9.]/g, ''); 
+              setNumParcelas(numericValue);
+            }}
+            style={styles.input}
+          />
+
+          <Text style={styles.label}>Juros:</Text>
+          <TextInput
+            placeholder='Informe os possíveis juros'
+            value={juros}
+            keyboardType="numeric"
+            onChangeText={(text) => {
+              const numericValue = text.replace(/[^0-9.]/g, ''); 
+              setJuros(numericValue);
+            }}
+            style={styles.input}
+          />
+
+          <Button title="Calcular"  onPress={() => {
+              calculo(juros, numParcelas);
+              setModalVisible(true);
+            }}/> 
+
+          <Modal
+            animationType="slide"
+            transparent={false}
+            visible={modalVisible}
+            onRequestClose={() => setModalVisible(!modalVisible)}
+          >
+            <View>
+                <Text style={styles.label}>Valor total do financiamento: R${resultadoCalculo}</Text> 
+                <Text style={styles.label}>Valor da entrada: R${valorEntrada}</Text> 
+                <Text style={styles.label}>Valor das Parcelas: R${valorParcela}</Text>  
+            </View>
+          </Modal>
+      </View>
+  );
+}
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="TelaInicial">
+        <Stack.Screen name="TelaInicial" component={TelaInicial} />
+        <Stack.Screen name="CalculoSemEntrada" component={CalculoSemEntrada} />
+        <Stack.Screen name="CalculoComEntrada" component={CalculoComEntrada} />
+        <Stack.Screen name="CalculoParcelasIguais" component={CalculoParcelasIguais} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
